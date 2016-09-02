4dcf9624bd90fe5708d5a3b97ef74c47ae6e3007
Mathieu Etienne <thiousi@gmail.com>
2016-09-02 15:39:27 +0200

    Added french translation
    This is a draft for french and may require updating once tested in-situ

---
+++ languages/fr.php
@@ -0,0 +1,10 @@
+<?php
+l::set('plugin.ratings.success', 'Vous avez voté!');
+l::set('plugin.ratings.vote.for', 'Voter pour');
+l::set('plugin.ratings.send', 'Voter');
+l::set('plugin.ratings.not.rated', 'Vous n'avez pas sélectionné de note!');
+l::set('plugin.ratings.average', 'Note');
+l::set('plugin.ratings.count', 'Votes');
+l::set('plugin.ratings.delete.blacklist', 'Supprimer la blacklist');
+l::set('plugin.ratings.delete.votes', 'Supprimer les votes');
+l::set('plugin.ratings.stars', 'étoiles');

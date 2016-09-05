3ab0ee749aff8fc6737e1f94a76b607210e0927d
Mathieu Etienne <thiousi@gmail.com>
2016-09-05 09:18:03 +0200

    Fixed issue with apostrophe

--- languages/fr.php
+++ languages/fr.php
@@ -2,7 +2,7 @@
 l::set('plugin.ratings.success', 'Vous avez voté!');
 l::set('plugin.ratings.vote.for', 'Voter pour');
 l::set('plugin.ratings.send', 'Voter');
-l::set('plugin.ratings.not.rated', 'Vous n'avez pas sélectionné de note!');
+l::set('plugin.ratings.not.rated', "Vous n'avez pas sélectionné de note!");
 l::set('plugin.ratings.average', 'Note');
 l::set('plugin.ratings.count', 'Votes');
 l::set('plugin.ratings.delete.blacklist', 'Supprimer la blacklist');
